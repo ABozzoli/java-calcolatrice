d7ac7d6413b575652bfddb68c06098c5e424a3a7
ABozzoli <alessandrobozzoli92@gmail.com>
2021-11-29 16:39:43 +0100

    Metodo sommaInt

---
+++ src/org/generation/italy/utils/CalcoliHelper.java
@@ -0,0 +1,17 @@
+package org.generation.italy.utils;
+
+public class CalcoliHelper {
+
+	private CalcoliHelper() {
+		// costruttore private così non posso creare istanze
+	}
+	
+	private int x;
+	private int y;
+
+	public static int sommaInt(int x, int y) {
+		int sommaInt = x + y;
+		return sommaInt;
+	}
+	
+}
