37d1789d47ce464380b21f6eda446c973ffa7a49
ABozzoli <alessandrobozzoli92@gmail.com>
2021-11-29 16:50:57 +0100

    Agg. differenza, moltiplica e valore assoluto

--- src/org/generation/italy/utils/CalcoliHelper.java
+++ src/org/generation/italy/utils/CalcoliHelper.java
@@ -14,4 +14,21 @@ public class CalcoliHelper {
 		return sommaInt;
 	}
 	
+	public static int differenzaInt(int x, int y) {
+		int differenzaInt = x - y;
+		return differenzaInt;
+	}
+	
+	public static int moltiplicaInt(int x, int y) {
+		int moltiplicaInt = x * y;
+		return moltiplicaInt;
+	}
+	
+	public static int valoreAssolutoInt(int x) {
+		if(x < 0) {
+			x = x * -1;
+		}
+		return x;
+	}
+	
 }
